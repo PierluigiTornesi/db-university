40b82ea7b3d86599e0c741563611518a126c482c
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-12-21 10:08:03 +0100

    update: cambio terza query

--- query.sql
+++ query.sql
@@ -9,9 +9,9 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 --3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT * FROM `students` 
-WHERE ((YEAR(CURRENT_DATE)) - (YEAR(`date_of_birth`))) > 30;
-
+SELECT *
+FROM `students`
+WHERE `date_of_birth` <= CURDATE() - INTERVAL 30 YEAR;
 
 --4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * 
